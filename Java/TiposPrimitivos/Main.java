fc019a00289642c8a48746cf172baceb735b9efe
Cargnieli-2016 <cargnieli2016@gmail.com>
2025-11-30 09:09:40 -0300

    tipos primitivos

---
+++ Java/TiposPrimitivos/Main.java
@@ -0,0 +1,15 @@
+package Java.TiposPrimitivos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte number = 127; // 1 byte
+        short number2 = 32767; // 2 bytes
+        int number3 = 2147483647; // 4 bytes
+        long number4 = 9223372036854775807L; // 8 bytes
+        float number5 = 65.4F; // 4 bytes
+        double number6 = 123456.789D; // 8 bytes
+        char character = 'A'; // 2 bytes
+        boolean condition = true; // 1 bit
+    }
+}
