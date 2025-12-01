3827a16f811625c2e96db7141ec50b3f567a4421
Cargnieli-2016 <cargnieli2016@gmail.com>
2025-11-30 21:14:09 -0300

    operadores de atribuição e lógicos

--- Java/OperadoreAtribuicaoELogicos/Main.java
+++ Java/OperadoreAtribuicaoELogicos/Main.java
@@ -14,8 +14,35 @@ public class Main {
         // Pode dirigir
         System.out.println("Quantos anos você tem ?");
         var age = scanner.nextInt();
-        var canDrive = age >= 18;
+
+        System.out.println("Você é emancipado?");
+        var isEmancipated = scanner.nextBoolean();
+        var canDrive = age >= 18 || (isEmancipated && age >= 16);
+
         System.out.println("Você pode dirigir? " + canDrive);
+
+        System.out.println("----------------------------------------");
+
+        System.out.printf("true  && true = %s \n", true && true);
+        System.out.printf("false && false = %s \n", false && false);
+
+        System.out.printf("true  && false = %s \n", true && false);
+        System.out.printf("false && true = %s \n", false && true);
+
+
+        System.out.println("----------------------------------------");
+
+        System.out.printf("true  || true = %s \n", true || true);
+        System.out.printf("false || false = %s \n", false || false);
+
+        System.out.printf("true  || false = %s \n", true || false);
+        System.out.printf("false || true = %s \n", false || true);
+
+        System.out.println("------------------------------------------");
+
+        System.out.printf("!true  = %s\n", !true);
+        System.out.printf("!false = %s\n", !false);
+
     }
 
 }
