dc65598b0c77af6a0a3998a98bc16455ed298870
Cargnieli-2016 <cargnieli2016@gmail.com>
2025-12-01 16:10:55 -0300

    exercicios

---
+++ Java/Exercicios/Main.java
@@ -0,0 +1,22 @@
+package Java.Exercicios;
+
+import java.time.OffsetDateTime;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var baseYear = OffsetDateTime.now().getYear();
+
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe o seu nome: ");
+        var name = scanner.next();
+
+        System.out.println("Informe o ano do seu nascimento: ");
+        var year = scanner.nextInt();
+
+        var age = baseYear - year;
+
+        System.out.printf("Olá %s você tem %s anos \n", name, age);
+
+    }
+}
