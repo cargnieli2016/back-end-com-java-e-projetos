bb632596017754a9782f7d1326c5f10cd7b5eef1
Cargnieli-2016 <cargnieli2016@gmail.com>
2025-12-01 08:58:06 -0300

    decremeneto e incremento

--- Java/OperadoresAritmeticos/Incremento/Main.java
+++ Java/OperadoresAritmeticos/Incremento/Main.java
@@ -3,8 +3,8 @@ package Java.OperadoresAritmeticos.Incremento;
 public class Main {
     public static void main(String[] args) {
         var value = 50;
-        System.out.println(10 + ++value);
-        System.out.println(10 + value);
+        System.out.println(++value);
+        System.out.println(value);
 
         System.out.println("===== Pós incremento ===== ");
         System.out.println(10 + value++);
