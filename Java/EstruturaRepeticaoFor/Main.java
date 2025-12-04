227626956410ef9eec8d658c2338d855bdba5b73
Cargnieli-2016 <cargnieli2016@gmail.com>
2025-12-04 14:24:05 -0300

    estrutura de repetição for

---
+++ Java/EstruturaRepeticaoFor/Main.java
@@ -0,0 +1,13 @@
+package Java.EstruturaRepeticaoFor;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        for (var i = 1; i <= 10 ; i++ ) {
+            System.out.println(i);
+
+        }
+    }
+}
